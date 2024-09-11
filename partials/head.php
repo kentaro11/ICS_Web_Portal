95b1b85456ba958c044108f649c6e4505bbbde61
Jelo Dimaano <jannangelodimaano@gmail.com>
2024-09-12 02:17:25 +0800

    Create header links

---
+++ partials/head.php
@@ -0,0 +1,3 @@
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet">
+<link rel="stylesheet" href="css/login_style.css">
+<link href="https://cdn.jsdelivr.net/npm/bootstrap-icons/font/bootstrap-icons.css" rel="stylesheet">
