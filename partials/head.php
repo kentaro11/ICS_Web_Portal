9b22560617103d67ae56db95623e20e70c34b366
Jelo Dimaano <jannangelodimaano@gmail.com>
2024-09-12 21:26:25 +0800

    Add link

--- partials/head.php
+++ partials/head.php
@@ -1,3 +1,4 @@
 <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet">
-<link rel="stylesheet" href="css/login_style.css">
+<link rel="stylesheet" href="css/loginStyle.css">
 <link href="https://cdn.jsdelivr.net/npm/bootstrap-icons/font/bootstrap-icons.css" rel="stylesheet">
+<link rel="stylesheet" href="css/header.css">
