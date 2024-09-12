e48fce0aadda5455e882848d343781dfd6c97af1
Jelo Dimaano <jannangelodimaano@gmail.com>
2024-09-12 20:21:05 +0800

    Add some modification

--- index.php
+++ index.php
@@ -1,21 +1,24 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
-  <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Login Page</title>
-  <?php include "partials/head.php"?>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Login Page</title>
+    <?php include "partials/head.php" ?>
 </head>
+
 <body>
     <div class="container d-flex justify-content-center align-items-center min-vh-100">
-        <div class="card shadow-lg" style="width: 35rem; border-radius: 20px;">
+        <div class="card shadow-lg" style="width: 50rem; height: 40rem; border-radius: 20px;">
             <div class="card-header text-center text-white position-relative">
+                <div class="z-2 rounded-top-4 overlay position-absolute top-50 start-50 translate-middle" style="width: 100%; height: 100%; background-color: rgba(0, 0, 0, 0.5);"></div>
                 <img src="img/school_banner.png" alt="Background" class="card-img-top bg-header img-fluid">
-                <img src="img/ics_logo.png" alt="Logo" class="position-absolute top-50 start-50 translate-middle" style="width: 100px; height: 100px;">
+                <img src="img/ics_logo.png" alt="Logo" class="z-3 position-absolute top-50 start-50 translate-middle" style="width: 100px; height: 100px;">
             </div>
 
             <div class="rounded-bottom-4 card-body form-body text-white">
-                <h3 class="text-center mb-4">LOGIN</h3>
+                <h1 class="text-center mb-4">LOGIN</h1>
                 <form>
                     <div class="mb-3">
                         <label for="userId" class="form-label">User ID</label>
@@ -29,17 +32,15 @@
                         <label for="password" class="form-label">Password</label>
                         <div class="input-group">
                             <span class="input-group-text"><i class="bi bi-lock"></i></span>
-                            <input type="password" class="form-control" id="password" placeholder="Enter Password">
-                            <span class="input-group-text" onclick="togglePassword()">
-                            <i class="bi bi-eye-slash" id="togglePasswordIcon"></i> 
-                            </span>
+                            <input type="password" class="rounded-end-2 form-control" id="password" placeholder="Enter Password" style="padding-right: 30px;">
+                            <i class="position-absolute top-50 end-0 translate-middle-y bi bi-eye-slash" style="color: black; padding-right: 8px;" id="togglePasswordIcon" onclick="togglePassword()"></i>
                         </div>
                         <small class="form-text text-light">* Password is case sensitive</small>
                         <a href="signUp" class="text-light d-block mt-2">Forgot Password?</a>
                     </div>
 
                     <div class="d-flex justify-content-center">
-                        <button type="submit" class="btn btn-warning" style="border-radius: 20px;">Sign In</button>
+                        <button type="submit" class="btn btn-warning" style="border-radius: 20px;"><i class="bi bi-box-arrow-in-right"></i> Sign In</button>
                     </div>
                 </form>
             </div>
@@ -47,5 +48,7 @@
     </div>
 
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js"></script>
+    <script src="js/eyeToggle.js"></script>
 </body>
+
 </html>
