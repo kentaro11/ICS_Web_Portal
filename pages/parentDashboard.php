135f38699498ad43ffc6abc81604754ca514c485
Jelo Dimaano <jannangelodimaano@gmail.com>
2024-09-12 21:26:17 +0800

    Create parent dashboard file

---
+++ pages/parentDashboard.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>ICS - Parent Dashboard</title>
+    <?php include "partials/head.php" ?>
+</head>
+<body>
+    <div class="container d-flex">
+        <div class="header">
+
+        </div>
+
+        <div class="body">
+
+        </div>
+
+        <div class="footer">
+
+        </div>
+    </div>    
+</body>
+</html>
