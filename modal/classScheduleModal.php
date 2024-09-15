a71faa847746cc250103c666e668d77fef2090ec
Jelo Dimaano <jannangelodimaano@gmail.com>
2024-09-15 17:55:23 +0800

    Add table and style

--- modal/classScheduleModal.php
+++ modal/classScheduleModal.php
@@ -10,16 +10,86 @@
 
 <body>
   <!-- Modal -->
-  <div class="modal fade" id="staticBackdrop" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1" aria-labelledby="staticBackdropLabel" aria-hidden="true">
+  <div class="modal fade modal-xl" id="staticBackdrop" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1" aria-labelledby="staticBackdropLabel" aria-hidden="true">
     <div class="modal-dialog">
       <div class="modal-content">
         <div class="modal-header">
-          <h1 class="modal-title" id="staticBackdropLabel" style="margin: 0 0 0 110px !important;">CLASS SCHEDULE</h1>
+          <h1 class="modal-title" id="staticBackdropLabel">CLASS SCHEDULE</h1>
           <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
         </div>
         <div class="modal-body">
           <div class="modal-dialog modal-dialog-centered" id="classSchedule">
-
+            <!-- Table -->
+            <table class="table table-bordered rounded">
+              <thead>
+                <tr>
+                  <th>Time</th>
+                  <th>Monday</th>
+                  <th>Tuesday</th>
+                  <th>Wednesday</th>
+                  <th>Thursday</th>
+                  <th>Friday</th>
+                </tr>
+              </thead>
+              <tbody>
+                <tr>
+                  <td>7:00 AM</td>
+                  <td>Mathematics<br>Mr. Juan C. Dela Cruz</td>
+                  <td>Mathematics<br>Mr. Juan C. Dela Cruz</td>
+                  <td></td>
+                  <td>English<br>Mr. Juan C. Dela Cruz</td>
+                  <td>English<br>Mr. Juan C. Dela Cruz</td>
+                </tr>
+                <tr>
+                  <td>8:00 AM</td>
+                  <td>Hekasi<br>Mr. Juan C. Dela Cruz</td>
+                  <td></td>
+                  <td></td>
+                  <td>Hekasi<br>Mr. Juan C. Dela Cruz</td>
+                  <td>Hekasi<br>Mr. Juan C. Dela Cruz</td>
+                </tr>
+                <tr>
+                  <td>9:00 AM</td>
+                  <td></td>
+                  <td>Hekasi<br>Mr. Juan C. Dela Cruz</td>
+                  <td>Filipino<br>Mr. Juan C. Dela Cruz</td>
+                  <td>Filipino<br>Mr. Juan C. Dela Cruz</td>
+                  <td>Mathematics<br>Mr. Juan C. Dela Cruz</td>
+                </tr>
+                <tr>
+                  <td>10:00 AM</td>
+                  <td>Science<br>Mr. Juan C. Dela Cruz</td>
+                  <td></td>
+                  <td>Mathematics<br>Mr. Juan C. Dela Cruz</td>
+                  <td>Mathematics<br>Mr. Juan C. Dela Cruz</td>
+                  <td>Filipino<br>Mr. Juan C. Dela Cruz</td>
+                </tr>
+                <tr>
+                  <td>11:00 AM</td>
+                  <td>English<br>Mr. Juan C. Dela Cruz</td>
+                  <td>English<br>Mr. Juan C. Dela Cruz</td>
+                  <td></td>
+                  <td>English<br>Mr. Juan C. Dela Cruz</td>
+                  <td></td>
+                </tr>
+                <tr>
+                  <td>1:00 PM</td>
+                  <td>Filipino<br>Mr. Juan C. Dela Cruz</td>
+                  <td>Filipino<br>Mr. Juan C. Dela Cruz</td>
+                  <td>Science<br>Mr. Juan C. Dela Cruz</td>
+                  <td></td>
+                  <td></td>
+                </tr>
+                <tr>
+                  <td>2:00 PM</td>
+                  <td>Science<br>Mr. Juan C. Dela Cruz</td>
+                  <td>Science<br>Mr. Juan C. Dela Cruz</td>
+                  <td></td>
+                  <td>Science<br>Mr. Juan C. Dela Cruz</td>
+                  <td>Science<br>Mr. Juan C. Dela Cruz</td>
+                </tr>
+              </tbody>
+            </table>
           </div>
         </div>
       </div>
