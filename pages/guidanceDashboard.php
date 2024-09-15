8a70a1bfc4ed000fbfe8f812701d9c3d6986cecd
Jelo Dimaano <jannangelodimaano@gmail.com>
2024-09-15 21:56:00 +0800

    Include announcement modal

--- pages/guidanceDashboard.php
+++ pages/guidanceDashboard.php
@@ -6,6 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>ICS - Parent Dashboard</title>
     <?php include "../partials/head.php" ?>
+    <?php include "../modal/announcementModal.php" ?>
     <link rel="stylesheet" href="../css/header.css">
     <link rel="stylesheet" href="../css/body.css">
     <link rel="stylesheet" href="../css/footer.css">
