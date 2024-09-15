12f289ae4312331f6dc8456111baef6815beacbb
Jelo Dimaano <jannangelodimaano@gmail.com>
2024-09-15 17:40:14 +0800

    Create class schedule modal

---
+++ modal/classScheduleModal.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Class Schedule - ICS Parent Portal</title>
+  <link rel="stylesheet" href="../css/classScheduleModal.css">
+</head>
+
+<body>
+  <!-- Modal -->
+  <div class="modal fade" id="staticBackdrop" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1" aria-labelledby="staticBackdropLabel" aria-hidden="true">
+    <div class="modal-dialog">
+      <div class="modal-content">
+        <div class="modal-header">
+          <h1 class="modal-title" id="staticBackdropLabel" style="margin: 0 0 0 110px !important;">CLASS SCHEDULE</h1>
+          <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+        </div>
+        <div class="modal-body">
+          <div class="modal-dialog modal-dialog-centered" id="classSchedule">
+
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</body>
+
+</html>
