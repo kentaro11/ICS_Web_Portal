19e369d40970a0f621196d8a6664869d9acbc74a
Jelo Dimaano <jannangelodimaano@gmail.com>
2024-09-15 21:57:24 +0800

    Modify title

--- modal/learningMaterialsModal.php
+++ modal/learningMaterialsModal.php
@@ -4,7 +4,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Class Schedule - ICS Parent Portal</title>
+    <title>Learning Materials - ICS Teacher Portal</title>
     <link rel="stylesheet" href="../css/inputGradesModal.css">
 </head>
 
