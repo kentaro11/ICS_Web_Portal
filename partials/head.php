9c2a8c8862794ad228b4572fb58de0ed1d6047c2
Jelo Dimaano <jannangelodimaano@gmail.com>
2024-09-15 17:39:49 +0800

    Add bootstrap js

--- partials/head.php
+++ partials/head.php
@@ -1,8 +1,14 @@
+<!-- Bootstrap CSS -->
 <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet">
 <link href="https://cdn.jsdelivr.net/npm/bootstrap-icons/font/bootstrap-icons.css" rel="stylesheet">
+
+<!-- Font -->
 <link rel="preconnect" href="https://fonts.googleapis.com">
 <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
 <link href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,100..900;1,100..900&display=swap" rel="stylesheet">
 
-<script src="https://code.iconify.design/iconify-icon/2.1.0/iconify-icon.min.js"></script>\\
-<script src="../js/modal.js"></script>
+<!-- Iconify -->
+<script src="https://code.iconify.design/iconify-icon/2.1.0/iconify-icon.min.js"></script>
+
+<!-- Bootstrap JS -->
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.min.js"></script> 
