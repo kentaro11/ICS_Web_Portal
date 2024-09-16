9592e8035bd82ff68d244ab170e9389337b103ea
Jelo Dimaano <jannangelodimaano@gmail.com>
2024-09-16 16:40:16 +0800

    Create student record modal php

---
+++ modal/studentRecordModal.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Announcement - ICS Guidance Portal</title>
+    <link rel="stylesheet" href="../css/classScheduleModal.css">
+</head>
+
+<body>
+    <!-- Modal -->
+    <div class="modal fade modal-xl" id="studentRecordModal" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1" aria-labelledby="staticBackdropLabel" aria-hidden="true">
+        <div class="modal-dialog">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h1 class="modal-title" id="staticBackdropLabel">ANNOUNCEMENT</h1>
+                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                </div>
+                <div class="modal-body">
+                    
+                </div>
+            </div>
+        </div>
+    </div>
+</body>
+
+</html>
