2a125999f47e4dd0a968c5cc622c10cd520df358
Jelo Dimaano <jannangelodimaano@gmail.com>
2024-09-16 16:52:25 +0800

    Add layout for student records

--- modal/studentRecordModal.php
+++ modal/studentRecordModal.php
@@ -14,11 +14,63 @@
         <div class="modal-dialog">
             <div class="modal-content">
                 <div class="modal-header">
-                    <h1 class="modal-title" id="staticBackdropLabel">ANNOUNCEMENT</h1>
+                    <h1 class="modal-title" id="staticBackdropLabel">STUDENT RECORD</h1>
                     <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
                 </div>
                 <div class="modal-body">
-                    
+                    <!-- Search Bar -->
+                    <div class="row search-bar">
+                        <div class="col-10">
+                            <input type="text" class="form-control" placeholder="Search Student">
+                        </div>
+                        <div class="col-2 d-flex justify-content-between">
+                            <button class="icon-btn">
+                                <img src="https://img.icons8.com/material-outlined/24/000000/upload.png" alt="Upload">
+                            </button>
+                            <button class="icon-btn">
+                                <img src="https://img.icons8.com/material-outlined/24/000000/synchronize.png" alt="Sync">
+                            </button>
+                            <button class="icon-btn">
+                                <img src="https://img.icons8.com/material-outlined/24/000000/filter.png" alt="Filter">
+                            </button>
+                        </div>
+                    </div>
+
+                    <!-- Table -->
+                    <div class="table-responsive">
+                        <table class="table table-bordered text-center">
+                            <thead class="table-warning">
+                                <tr>
+                                    <th scope="col">LRN</th>
+                                    <th scope="col">Name</th>
+                                    <th scope="col">Sex</th>
+                                    <th scope="col">Grade</th>
+                                    <th scope="col">Section</th>
+                                    <th scope="col">Address</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                <!-- Empty rows for now -->
+                                <tr>
+                                    <td></td>
+                                    <td></td>
+                                    <td></td>
+                                    <td></td>
+                                    <td></td>
+                                    <td></td>
+                                </tr>
+                                <tr>
+                                    <td></td>
+                                    <td></td>
+                                    <td></td>
+                                    <td></td>
+                                    <td></td>
+                                    <td></td>
+                                </tr>
+                                <!-- Add more rows as needed -->
+                            </tbody>
+                        </table>
+                    </div>
                 </div>
             </div>
         </div>
