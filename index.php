ffa17e29a01a54637f16428ce3ec88d12955ad27
Jelo Dimaano <jannangelodimaano@gmail.com>
2024-09-19 16:21:57 +0800

    Modify css link and add border radius to header

--- index.php
+++ index.php
@@ -6,7 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Login Page</title>
     <?php include "partials/head.php" ?>
-    <link rel="stylesheet" href="../css/loginStyle.css">
+    <link rel="stylesheet" href="css/loginStyle.css">
 </head>
 
 <body>
@@ -14,7 +14,7 @@
         <div class="card shadow-lg" style="width: 50rem; height: 40rem; border-radius: 20px;">
             <div class="card-header text-center text-white position-relative">
                 <div class="z-2 rounded-top-4 overlay position-absolute top-50 start-50 translate-middle" style="width: 100%; height: 100%; background-color: rgba(0, 0, 0, 0.5);"></div>
-                <img src="img/schoolBanner.jpg" alt="Background" class="card-img-top bg-header img-fluid">
+                <img src="img/schoolBanner.jpg" alt="Background" class="card-img-top bg-header img-fluid rounded-top-4">
                 <img src="img/icsLogo.png" alt="Logo" class="z-3 position-absolute top-50 start-50 translate-middle" style="width: 20%;">
             </div>
 
