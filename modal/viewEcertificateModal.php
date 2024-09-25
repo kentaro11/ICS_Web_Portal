53829e46c146a0ae4c2618c3cfb084a344494302
Jelo Dimaano <jannangelodimaano@gmail.com>
2024-09-26 02:10:27 +0800

    Modify layout

--- modal/viewEcertificateModal.php
+++ modal/viewEcertificateModal.php
@@ -11,9 +11,9 @@
 <body>
     <!-- Modal -->
     <div class="modal fade modal-xl" id="viewEcertModal" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1" aria-labelledby="staticBackdropLabel" aria-hidden="true">
-        <div class="modal-dialog">
+        <div class="modal-dialog modal-dialog-centered">
             <div class="modal-content">
-                <div class="modal-header">
+                <div class="modal-header text-center">
                     <h1 class="modal-title" id="staticBackdropLabel">E-CERTIFICATE</h1>
                     <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
                 </div>
