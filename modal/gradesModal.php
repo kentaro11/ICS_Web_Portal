39664a59121f328e1ec539c6f24607571d67adf8
Jelo Dimaano <jannangelodimaano@gmail.com>
2024-09-25 11:07:41 +0800

    Create a grades modal

---
+++ modal/gradesModal.php
@@ -0,0 +1,90 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Grades - ICS Parent Portal</title>
+    <link rel="stylesheet" href="../css/classScheduleModal.css">
+</head>
+
+<body>
+    <!-- Modal -->
+    <div class="modal fade modal-xl" id="gradesModal" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1" aria-labelledby="staticBackdropLabel" aria-hidden="true">
+        <div class="modal-dialog">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h1 class="modal-title" id="staticBackdropLabel">GRADES</h1>
+                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                </div>
+                <div class="modal-body">
+                    <!-- Table -->
+                    <table class="table table-bordered rounded">
+                        <thead>
+                            <tr>
+                                <th>Subject</th>
+                                <th>First</th>
+                                <th>Second</th>
+                                <th>Third</th>
+                                <th>Fourth</th>
+                                <th>Teacher</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <tr>
+                                <th>English</th>
+                                <td>90</td>
+                                <td>90</td>
+                                <td>90</td>
+                                <td>90</td>
+                                <td>Mr. Juan Dela Cruz</td>
+                            </tr>
+                            <tr>
+                                <th>Math</th>
+                                <td>90</td>
+                                <td>90</td>
+                                <td>90</td>
+                                <td>90</td>
+                                <td>Mr. Juan Dela Cruz</td>
+                            </tr>
+                            <tr>
+                                <th>Hekasi</th>
+                                <td>90</td>
+                                <td>90</td>
+                                <td>90</td>
+                                <td>90</td>
+                                <td>Mr. Juan Dela Cruz</td>
+                            </tr>
+                            <tr>
+                                <th>Filipino</th>
+                                <td>90</td>
+                                <td>90</td>
+                                <td>90</td>
+                                <td>90</td>
+                                <td>Mr. Juan Dela Cruz</td>
+                            </tr>
+                            <tr>
+                                <th>PE</th>
+                                <td>90</td>
+                                <td>90</td>
+                                <td>90</td>
+                                <td>90</td>
+                                <td>Mr. Juan Dela Cruz</td>
+                            </tr>
+                            <tr>
+                                <th>Science</th>
+                                <td>90</td>
+                                <td>90</td>
+                                <td>90</td>
+                                <td>90</td>
+                                <td>Mr. Juan Dela Cruz</td>
+                            </tr>
+                        </tbody>
+                    </table>
+                </div>
+            </div>
+        </div>
+    </div>
+</body>
+
+</html>
