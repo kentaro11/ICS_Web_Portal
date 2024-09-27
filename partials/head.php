c4c602ca78b6a015e5e551a749cb81f482fdb0c3
Jelo Dimaano <jannangelodimaano@gmail.com>
2024-09-27 19:19:33 +0800

    Add sweetalert link

--- partials/head.php
+++ partials/head.php
@@ -16,6 +16,9 @@
 <!-- JS for jQuery -->
 <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
 
+<!-- JS for Sweetalert -->
+<script src="https://unpkg.com/sweetalert/dist/sweetalert.min.js"></script>
+
 <!-- JS for full calender -->
 <script src='https://cdn.jsdelivr.net/npm/fullcalendar@6.1.15/index.global.min.js'></script>
 <script>
