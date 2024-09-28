cf2ce900917b98aa671783ed48d6192c731e4fd9
Jelo Dimaano <jannangelodimaano@gmail.com>
2024-09-28 16:36:36 +0800

    Modify table structure and display data in the table

--- modal/studentRecordModal.php
+++ modal/studentRecordModal.php
@@ -1,80 +1,82 @@
-<!DOCTYPE html>
-<html lang="en">
+<link rel="stylesheet" href="../css/modal.css">
 
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Student Record - ICS Guidance Portal</title>
-    <link rel="stylesheet" href="../css/classScheduleModal.css">
-</head>
+<!-- Modal -->
+<div class="modal fade modal-xl" id="studentRecordModal" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1" aria-labelledby="staticBackdropLabel" aria-hidden="true">
+    <div class="modal-dialog modal-dialog-centered">
+        <div class="modal-content" style="height: 700px !important;">
+            <div class="modal-header justify-content-center" style="border-bottom: none; height: 100px !important; padding: 0 !important;">
+                <h1 class="modal-title" id="staticBackdropLabel">STUDENT RECORD</h1>
+                <button type="button" class="btn-close btn-close position-absolute top-0 end-0" style="top: 25px !important; right: 25px !important;" data-bs-dismiss="modal" aria-label="Close"></button>
+            </div>
+            <div class="modal-body">
 
-<body>
-    <!-- Modal -->
-    <div class="modal fade modal-xl" id="studentRecordModal" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1" aria-labelledby="staticBackdropLabel" aria-hidden="true">
-        <div class="modal-dialog">
-            <div class="modal-content">
-                <div class="modal-header">
-                    <h1 class="modal-title" id="staticBackdropLabel">STUDENT RECORD</h1>
-                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-                </div>
-                <div class="modal-body">
-                    <!-- Search Bar -->
-                    <div class="row search-bar">
-                        <div class="col-10">
-                            <input type="text" class="form-control" placeholder="Search Student">
-                        </div>
-                        <div class="col-2 d-flex justify-content-between">
-                            <button class="icon-btn">
-                                <img src="https://img.icons8.com/material-outlined/24/000000/upload.png" alt="Upload">
-                            </button>
-                            <button class="icon-btn">
-                                <img src="https://img.icons8.com/material-outlined/24/000000/synchronize.png" alt="Sync">
-                            </button>
-                            <button class="icon-btn">
-                                <img src="https://img.icons8.com/material-outlined/24/000000/filter.png" alt="Filter">
-                            </button>
-                        </div>
-                    </div>
+                <!-- Table -->
 
-                    <!-- Table -->
-                    <div class="table-responsive">
-                        <table class="table table-bordered text-center">
-                            <thead class="table-warning">
-                                <tr>
-                                    <th scope="col">LRN</th>
-                                    <th scope="col">Name</th>
-                                    <th scope="col">Sex</th>
-                                    <th scope="col">Grade</th>
-                                    <th scope="col">Section</th>
-                                    <th scope="col">Address</th>
-                                </tr>
-                            </thead>
-                            <tbody>
-                                <!-- Empty rows for now -->
-                                <tr>
-                                    <td></td>
-                                    <td></td>
-                                    <td></td>
-                                    <td></td>
-                                    <td></td>
-                                    <td></td>
-                                </tr>
-                                <tr>
-                                    <td></td>
-                                    <td></td>
-                                    <td></td>
-                                    <td></td>
-                                    <td></td>
-                                    <td></td>
-                                </tr>
-                                <!-- Add more rows as needed -->
-                            </tbody>
-                        </table>
-                    </div>
+                <div class="table-responsive m-2">
+
+                    <table id="studentRecord" class="table table-bordered text-start">
+                        <thead style="background-color: var(--gold);">
+                            <tr>
+                                <th>LRN</th>
+                                <th>Name</th>
+                                <th>Sex</th>
+                                <th>Academic Year</th>
+                                <th>Grade Level</th>
+                                <th>Section</th>
+                                <th>Parent Name</th>
+                            </tr>
+                        </thead>
+                        <tbody id="tableBody" id="dataTable" style="max-height: 400px;">
+                            <?php
+                            include "../connectDb.php";
+
+                            $query = "SELECT CONCAT(s.first_name, ' ', s.middle_name, ' ', s.last_name) AS full_name,
+                                                        s.lrn AS lrn,
+                                                        s.sex AS sex,
+                                                        s.academic_year AS academic_year,
+                                                        gl.grade_level AS grade_level,
+                                                        sec.section_name AS section_name,
+                                                CONCAT(p.first_name, ' ', p.middle_name, ' ', p.last_name) AS parent_name
+                                                
+                                                FROM student s
+                                                LEFT JOIN section sec ON s.section_id = sec.section_id
+                                                LEFT JOIN grade_level gl ON s.grade_level_id = gl.grade_level_id
+                                                LEFT JOIN parent p ON s.parent_id = p.parent_id
+                                                WHERE s.lrn IS NOT NULL";
+
+                            $result = mysqli_query($conn, $query);
+                            if (mysqli_num_rows($result) > 0) {
+                                while ($row = mysqli_fetch_assoc($result)) {
+                                    echo "<tr>";
+                                    echo "<td>" . htmlspecialchars($row['lrn']) . "</td>";
+                                    echo "<td>" . htmlspecialchars($row['full_name']) . "</td>";
+                                    echo "<td class='text-capitalize'>" . htmlspecialchars($row['sex']) . "</td>";
+                                    echo "<td>" . htmlspecialchars($row['academic_year']) . "</td>";
+                                    echo "<td>" . htmlspecialchars($row['grade_level']) . "</td>";
+                                    echo "<td>" . htmlspecialchars($row['section_name']) . "</td>";
+                                    echo "<td>" . htmlspecialchars($row['parent_name']) . "</td>";
+                                    echo "</tr>";
+                                }
+                            } else {
+                                echo "<tr><td colspan='4'>No records found.</td></tr>";
+                            }
+                            ?>
+                        </tbody>
+                    </table>
                 </div>
             </div>
         </div>
     </div>
-</body>
+</div>
 
-</html>
+<script>
+    $(document).ready(function() {
+        $('#studentRecord').DataTable({
+            "paging": true, // Enables pagination
+            "searching": true, // Enables searching
+            "ordering": false, // Enables column sorting
+            "info": true, // Displays information about the table
+            "select": true,
+        });
+    });
+</script>
