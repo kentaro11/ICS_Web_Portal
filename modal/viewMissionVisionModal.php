a07b6fc1490cec0e8ce79fcff7cc4fdeba6421e8
Jelo <jannangelodimaano@gmail.com>
2024-10-02 03:18:43 +0800

    Create view mission vision modal

---
+++ modal/viewMissionVisionModal.php
@@ -0,0 +1,38 @@
+<link rel="stylesheet" href="../css/modal.css">
+
+<!-- Modal -->
+<div class="modal fade" id="viewMissionVision" tabindex="-1" aria-labelledby="staticBackdropLabel" aria-hidden="true">
+    <div class="modal-dialog modal-dialog-centered modal-lg">
+        <div class="modal-content" style="height: 600px !important; padding: 0 0 0 50px;">
+            <div class="modal-header justify-content-center" style="border-bottom: none; height: 100px !important; padding: 0 !important;">
+                <button type="button" class="btn-close position-absolute top-0 end-0" style="top: 25px !important; right: 25px !important;" data-bs-dismiss="modal" aria-label="Close"></button>
+            </div>
+            <div class="modal-body">
+                <div class="row">
+                    <div class="row">
+                        <h1 class="text-center">
+                            MISSION
+                        </h1>
+                    </div>
+                    <div class="row">
+                        <p class="lh-sm" style="color: var(--black);">
+                            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+                        </p>
+                    </div>
+                </div>
+                <div class="row mb-5">
+                    <div class="row">
+                        <h1 class="text-center">
+                            VISION
+                        </h1>
+                    </div>
+                    <div class="row">
+                        <p class="lh-sm" style="color: var(--black);">
+                            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+                        </p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
