4ea985b2a203ef15768a1f679285261428dbf0ef
Jelo <jannangelodimaano@gmail.com>
2024-10-02 05:26:24 +0800

    Modify id name

--- modal/uploadScheduleModal.php
+++ modal/uploadScheduleModal.php
@@ -1,5 +1,5 @@
 <link rel="stylesheet" href="../css/modal.css">
-<script src="../js/gradeLevel.js"></script>
+<script src="../js/gradeLevelSchedule.js"></script>
 
 <!-- Modal -->
 <div class="modal fade" id="uploadSchedule" tabindex="-1" aria-labelledby="staticBackdropLabel" aria-hidden="true">
@@ -13,22 +13,22 @@
                 <div class="form-container">
                     <form id="uploadScheduleForm" action="../function/.php" method="POST" enctype="multipart/form-data">
                         <div class="mb-3">
-                            <label for="gradeLevel" class="form-label">Grade Level</label>
-                            <select class="form-select" id="gradeLevel" name="grade_level" required>
+                            <label for="gradeLevelSchedule" class="form-label">Grade Level</label>
+                            <select class="form-select" id="gradeLevelSchedule" name="grade_level" required>
                                 <option selected disabled>Select</option>
                             </select>
                         </div>
 
                         <div class="mb-3">
-                            <label for="section" class="form-label">Section</label>
-                            <select class="form-select" id="section" name="section" required>
+                            <label for="sectionSchedule" class="form-label">Section</label>
+                            <select class="form-select" id="sectionSchedule" name="section" required>
                                 <option selected disabled>Select</option>
                             </select>
                         </div>
 
                         <div class="mb-3">
                             <label for="section" class="form-label">Schedule</label>
-                            <input class="form-control" type="file" id="uploadSchedule" name="upload-schedule" required>
+                            <input class="form-control" type="file" id="uploadSchedule" name="upload-schedule" accept=".xls, .xlsx" required>
                         </div>
 
                         <button type="submit" class="btn btn-primary w-100">Submit</button>
