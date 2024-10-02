e5e43b7f57a08459ed25fe273ff7881d16bfd669
Jelo <jannangelodimaano@gmail.com>
2024-10-03 01:35:11 +0800

    Replace script in head tag

--- index.php
+++ index.php
@@ -7,6 +7,7 @@
     <title>Login Page</title>
     <?php include "partials/head.php" ?>
     <link rel="stylesheet" href="css/loginStyle.css">
+    <script src="js/eyeToggle.js"></script>
 </head>
 
 <body>
@@ -47,9 +48,6 @@
             </div>
         </div>
     </div>
-
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js"></script>
-    <script src="js/eyeToggle.js"></script>
 </body>
 
 </html>
