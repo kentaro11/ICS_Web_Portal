5ef9bb726ca67b4ca2fba78e8c779d60c4d27b66
Jelo <jannangelodimaano@gmail.com>
2024-10-19 05:52:01 +0800

    Add full name, rank name, and swal

--- function/createAnnouncement.php
+++ function/createAnnouncement.php
@@ -4,6 +4,9 @@ include "../connectDb.php";
 
 // Check if the form is submitted
 if ($_SERVER['REQUEST_METHOD'] == 'POST') { 
+
+    $full_name = $conn->real_escape_string($_SESSION['full_name']);
+    $rank_name = $conn->real_escape_string($_SESSION['rank_name']);
     // Sanitize input to prevent SQL injection
     $announcement_title = $conn->real_escape_string($_POST['announcement_title']);
     $announcement_text = $conn->real_escape_string($_POST['announcement_text']);
@@ -16,32 +19,31 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
 
     try {
 
-        $insert_announcement_sql = "INSERT INTO announcements (title, announcement_text, announcement_file) 
-                                    VALUES ('$announcement_title', '$announcement_text', '$announcement_file')";
+        $insert_announcement_sql = "INSERT INTO announcements (title, announcement_text, announcement_file, full_name, rank_name) 
+                                    VALUES ('$announcement_title', '$announcement_text', '$announcement_file', '$full_name', '$rank_name')";
 
         if ($conn->query($insert_announcement_sql) !== TRUE) {
             throw new Exception("Error inserting into announcements: " . $conn->error);
         }
 
         $conn->commit();
-
-        // Set a success message and redirect
-        $_SESSION['success_message'] = "Announcement uploaded!";
-        header("Location: ../pages/guidanceDashboard.php");
+        $_SESSION['swal_message'] = [
+            'type' => 'success',
+            'title' => 'Announcement uploaded!',
+        ];
     } catch (Exception $e) {
         // If there is an error, rollback the transaction
         $conn->rollback();
 
-        // Set an error message and redirect back to the form
-        $_SESSION['error_message'] = $e->getMessage();
-        header("Location: ../pages/guidanceDashboard.php");
+        $_SESSION['swal_message'] = [
+            'type' => 'error',
+            'title' => $e->getMessage(),
+        ];
     }
 
-} else {
-    // If accessed without POST request, redirect back
-    $_SESSION['error_message'] = "Invalid request method.";
-    header("Location: ../pages/guidanceDashboard.php");
-}
+    header("Location: ../pages/guidanceDashboard");
+} 
+
 
 // Close the connection
 $conn->close();
